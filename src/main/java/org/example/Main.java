d9832902dd13d2c6426c8b3a21b07bcddb5c5dfb
maoxianjia <2103259533@qq.com>
2024-01-11 20:28:29 +0800

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author:lck
+ * @Date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
